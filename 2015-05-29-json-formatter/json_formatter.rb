3c24ac10b7c91b83be51b54833d2b45c796c628b
tiredpixel <tp@tiredpixel.com>
2015-05-29 17:44:14 +0100

    Fix json_formatter for hash sort, tweaking.

--- 2015-05-29-json-formatter/json_formatter.rb
+++ 2015-05-29-json-formatter/json_formatter.rb
@@ -9,7 +9,14 @@ input = $stdin.read
 
 def sort(data)
   if data.is_a?(Array)
-    data.sort.map { |e|
+    begin
+      data_s = data.sort
+    rescue ArgumentError
+      # comparison of Hash with Hash failed
+      data_s = data
+    end
+    
+    data_s.map { |e|
       sort(e)
     }
   elsif data.is_a?(Hash)
@@ -27,4 +34,4 @@ data = sort(data)
 output = JSON.pretty_generate(data)
 
 
-puts output
+print output
